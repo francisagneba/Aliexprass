7c0149a5107294e61ab9a056884ec82a36eaaddd
fagneba <francis.agneba@gmail.com>
2023-01-04 18:39:02 +0100

    Traitement du formulaire de la page checkout

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -11,19 +11,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CheckoutController extends AbstractController
 {
+    private $cartServices;
+
+    public function __construct(CartServices $cartServices)
+    {
+        $this->cartServices = $cartServices;
+    }
     /**
      * @Route("/checkout", name="app_checkout")
      */
-    public function index(CartServices $cartServices, Request $request): Response
+    public function index( Request $request): Response
     {
          //On recupère l'utilisateur
          $user = $this->getUser();
 
          //On recupère le panier
-         $cart = $cartServices->getFullCart();
+         $cart = $this->cartServices->getFullCart();
 
-         //si le panier est vide, on le redirige vers la page d'accueil
-         if(!$cart){
+         //si ce n'est pas definit alors le panier est vide, on le redirige vers la page d'accueil
+         if(!isset($cart['products'])){
             return $this->redirectToRoute("app_home");
          }
 
@@ -37,10 +43,8 @@ class CheckoutController extends AbstractController
          //Nous allons initialiser le formulaire
          $form = $this->createForm(CheckoutType::class,null,['user'=>$user]);
 
-         // On va annalyser la requeteet ajoutant Request dans la fuction index
+         // On va annalyser la requete an ajoutant Request dans la fuction index
          $form->handleRequest($request);
-
-         //traitement du formulaire
 
         return $this->render('checkout/index.html.twig',[
 
@@ -48,5 +52,59 @@ class CheckoutController extends AbstractController
             'checkout' => $form->createView()
 
         ]);
+    }
+    
+    //traitement du formulaire 
+
+    /**
+     * @Route("/checkout/confirm", name="app_checkout_confirm")
+     */
+    public function confirm(Request $request): Response {
+        //On recupère l'utilisateur
+        $user = $this->getUser();
+
+        //On recupère le panier
+        $cart = $this->cartServices->getFullCart();
+
+         //si ce n'est pas definit alors le panier est vide, on le redirige vers la page d'accueil
+         if(!isset($cart['products'])){
+            return $this->redirectToRoute("app_home");
+         }
+
+         //Si l'utilisateur n'a pas d'adresse on le redirige vers la page d'ajout d'adresse.
+         if (!$user->getAddresses()->getValues()) {
+            //on ajoute un message flash avant de rediriger vers la page d'ajut d'adresse
+            $this->addFlash('checkout_message', 'please add an address to your account without continuing');
+            return $this->redirectToRoute("app_address_new");
+         }
+
+         //Nous allons initialiser le formulaire
+         $form = $this->createForm(CheckoutType::class,null,['user'=>$user]);
+
+         // On va annalyser la requete an ajoutant Request dans la fuction index
+         $form->handleRequest($request);
+
+         if($form->isSubmitted() && $form->isValid()){
+
+            $data = $form->getData();
+
+            $address = $data['address'];
+            $carrier = $data['carrier'];
+            $informations = $data['informations'];
+
+            return $this->render('checkout/confirm.html.twig',[
+
+                'cart' => $cart,
+                'address' => $address,
+                'carrier' => $carrier,
+                'informations' => $informations, 
+                'checkout' => $form->createView()
+    
+            ]);
+
+         }
+
+         return $this->redirectToRoute("app_checkout");
+
     }
 }
